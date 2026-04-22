2246743f66e32a482507f501c79cd9814c901d26
shcmal-chel <shcmalivan@gmail.com>
2026-04-22 09:56:25 +0300

    10

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -6,5 +6,25 @@ void main(){
 }
 
 class GradientContainer extends StatelessWidget {
-
+  const GradientContainer({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return  Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Colors.white,
+              Colors.blue,
+              Colors.red
+            ],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: Center(
+          child: Text(
+            "Hello world!"),
+            ),
+          );
+  }
 }
---
+++ lib/styled_text.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class StyledText extends StatelessWidget {
+  const StyledText({super.key});
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+          child: Text(
+            "ПАШЕЛ НАФИГ"),
+            ),
+    )
+  }
+}
