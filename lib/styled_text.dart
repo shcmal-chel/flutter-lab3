18c566d8f67cc4914e68bfb47d2b7716c16b605c
shcmal-chel <shcmalivan@gmail.com>
2026-04-29 08:55:30 +0300

    Extract StyledText into a separate widget and file

--- lib/styled_text.dart
+++ lib/styled_text.dart
@@ -3,11 +3,12 @@ import 'package:flutter/material.dart';
 class StyledText extends StatelessWidget {
   const StyledText({super.key});
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-          child: Text(
-            "ПАШЕЛ НАФИГ"),
+    return Text(
+            "ПАШЕЛ НАФИГ",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 32,
             ),
-    )
+    );
   }
 }
