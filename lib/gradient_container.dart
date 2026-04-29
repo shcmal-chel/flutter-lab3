639abd7c986812195423758b1459ab5aaed0d712
shcmal-chel <shcmalivan@gmail.com>
2026-04-29 09:07:12 +0300

    Make GradientContainer reusable by accepting colors as a parameter

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,34 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_lab3_app_shmal/styled_text.dart';
 
-
-void main(){
-
-}
+void main() {}
 
 const startAlignment = Alignment.topCenter;
 const endAlignment = Alignment.bottomCenter;
 
 class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
+  final Color color1;
+  final Color color2;
+  final Color color3;
+
+  const GradientContainer(
+    this.color1,
+    this.color2,
+    this.color3, {
+    super.key,
+  });
   @override
   Widget build(BuildContext context) {
-    return  Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [
-              Colors.white,
-              Colors.blue,
-              Colors.red
-            ],
-            begin: startAlignment,
-            end: endAlignment,
-          ),
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [color1, color2, color3],
+          begin: startAlignment,
+          end: endAlignment,
         ),
-        child: Center(
-          child: StyledText(
-            "Hello world!"),
-            ),
-          );
+      ),
+      child: Center(
+        child: StyledText("Hello world!"),
+      ),
+    );
   }
 }
